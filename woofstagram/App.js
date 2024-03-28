9951c946bc695b70b1658978f249c1ca6de610b6
gabriel <gabrielipcarvalho@gmail.com>
2024-03-28 13:10:30 +1000

    advancements on trends section, some corrections on headings

--- woofstagram/App.js
+++ woofstagram/App.js
@@ -13,7 +13,6 @@ const HomeScreen = () => (
     <Header label = "Trending Woofs"></Header>
       <Trends></Trends>
     <Header label = "New Woof Posts"></Header>
-      <Trends></Trends>
   </View>
 );
 
@@ -28,41 +27,170 @@ const Header = (props) => {
 };
 
 const Trends = () => (
-  <ScrollView horizontal = {true} style = {styles.trends}>
-    <Cards></Cards>
-  </ScrollView>
+  <View style = {styles.trends}>
+    <ScrollView horizontal = {true}>
+    	{data.woofs.map((woof) => 
+	  		<Cards 
+				key = {woof.id} 
+				woofName = {woof.name}
+				avatar = {woof.avatar}
+			/> )}
+    </ScrollView>
+  </View>
+
 );
 
-const Cards = () => {
+const Cards = (props) => {
   return (
-    <View style = {styles.cards}></View>
+    <View style = {styles.cards}>
+		<Image 
+			source = {{uri: props.avatar}} 
+			style = {styles.cardsAvatar}	
+		/>
+		<Text style = {styles.cardsText}>
+		{props.woofName}
+		</Text>
+	</View>
   );
 }
 
 const styles = StyleSheet.create({
   homeScreen: {
     flex: 1,
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
+	backgroundColor: '#FAF9FA',
   },
   header: {
-    flex: 0,
+    flex: 0.0615,
     backgroundColor: 'pink',
-    // flexDirection: 'row',
+    flexDirection: 'row',
+	justifyContent:'flex-start',
+	alignItems: 'flex-end'
+
     },
   headerText: {
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: '600',
+	// fontFamily: 'Kanit',
+	paddingLeft: 10,
+	paddingBottom: 3,
   },
   trends: {
-    flex: 1,
+    flex: 0.1675,
     flexDirection: 'row',
     backgroundColor: "green",
+	justifyContent: 'flex-start',
+	alignItems: 'center',
+	padding: 12,
   },
   cards : {
-    // backgroundColor: 'black',
-    // width: 40
+    backgroundColor: 'black',
+    width: 78,
+    height: 102,
+	marginRight: 12,
+	borderWidth: 1,
+	borderColor: '#E7E3EB',
+	borderRadius: 10,
+	padding: 8,
   },
+  cardsText: {
+	color: "white",
+  },
+  cardsAvatar: {
+	flex: 1,
+	borderRadius: 100,
+  }
 
   });
+
+const data = {
+	woofs: [
+		{
+		id: 'woof-1', 
+		name: 'Rex', 
+		avatar: 'https://images.unsplash.com/photo-1558788353-f76d92427f16?auto=format&fit=crop&w=648&q=80',
+		caretaker: 'Victor Grabarczyk',
+		source: 'https://unsplash.com/photos/x5oPmHmY3kQ',
+		},
+		{
+		id: 'woof-2', 
+		name: 'Ball', 
+		avatar: 'https://images.unsplash.com/photo-1585584114963-503344a119b0?auto=format&fit=crop&h=64&q=80',
+		caretaker: 'Tatiana Rodriguez',
+		source: 'https://unsplash.com/photos/J40C1k6Fut0',
+		},
+		{
+		id: 'woof-3', 
+		name: 'Happy', 
+		avatar: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?auto=format&fit=crop&h=64&q=80',
+		caretaker: 'Marliese Streefland',
+		source: 'https://unsplash.com/photos/2l0CWTpcChI',
+		},
+		{
+		id: 'woof-4',
+		name: 'Fluffy',
+		avatar: 'https://images.unsplash.com/photo-1554956615-1ba6dc39921b?auto=format&fit=crop&h=64&q=80',
+		caretaker: 'Nick Fewings',
+		source: 'https://unsplash.com/photos/rMKXLAIa2OY',
+		},
+		{
+		id: 'woof-5',
+		name: 'Spirit',
+		avatar: 'https://images.unsplash.com/photo-1514984879728-be0aff75a6e8?auto=format&fit=crop&h=64&q=80',
+		caretaker: 'Jamie Street',
+		source: 'https://unsplash.com/photos/uNNCs5kL70Q',
+		},
+	],
+	posts: [
+		{
+		id: 'post-1',
+		image: 'https://images.unsplash.com/photo-1544568100-847a948585b9?auto=format&fit=crop&w=967&q=80',
+		title: 'Happy Woofs',
+		description: 'How to keep your woof health and happy. We\'ve asked some of the best experts out there.',
+		caretaker: 'Jamie Street',
+		source: 'https://unsplash.com/photos/UtrE5DcgEyg',
+		},
+		{
+		id: 'post-2',
+		image: 'https://images.unsplash.com/photo-1450778869180-41d0601e046e?auto=format&fit=crop&w=850&q=80',
+		title: 'Woofs & friends',
+		description: 'Best friends are important for humans, but also for dogs.',
+		caretaker: 'Krista Mangulsone',
+		source: 'https://unsplash.com/photos/9gz3wfHr65U',
+		},
+		{
+		id: 'post-3',
+		image: 'https://images.unsplash.com/photo-1558947530-cbcf6e9aeeae?auto=format&fit=crop&w=634&q=80',
+		title: 'Good Woofs',
+		description: 'A good woof is a woof that brings joy. Here are a few tips to let your woof behave.',
+		caretaker: 'Olia Nayda',
+		source: 'https://unsplash.com/photos/f6v_Q0WXEK8',
+		},
+		{
+		id: 'post-4',
+		image: 'https://images.unsplash.com/photo-1444212477490-ca407925329e?auto=format&fit=crop&w=1100&q=80',
+		title: 'Wild Woofs',
+		description: 'In some parts of the world, wild woofs are very common. Learn how to interact in a nice way.',
+		caretaker: 'Anoir Chafik',
+		source: 'https://unsplash.com/photos/2_3c4dIFYFU',
+		},
+		{
+		id: 'post-5',
+		image: 'https://images.unsplash.com/photo-1567014543648-e4391c989aab?auto=format&fit=crop&w=1050&q=80',
+		title: 'Sleepy Woofs',
+		description: 'Sleeping is just as important for woofs as it is for humans. What are the main things your woof dreams about.',
+		caretaker: 'Max Singh',
+		source: 'https://unsplash.com/photos/2637Pic9xMw',
+		},
+		{
+		id: 'post-6',
+		image: 'https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?auto=format&fit=crop&w=967&q=80',
+		title: 'Exploring Woofs',
+		description: 'Just sitting in one place is boring for most woofs. How do woofs explore the world?',
+		caretaker: 'Jamie Street',
+		source: 'https://unsplash.com/photos/wcO2PWLuQ3U',
+		},
+	],
+};
 
 export default App;
