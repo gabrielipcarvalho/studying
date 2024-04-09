f46a1d63242e3503ec5c52f1a63c22aa6214555a
gabriel <gabrielipcarvalho@gmail.com>
2024-04-09 15:50:54 +1000

    included search functionality

--- react/monster-rolodex/src/App.js
+++ react/monster-rolodex/src/App.js
@@ -1,44 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { Component } from 'react';
+import { Component } from "react";
 
 class App extends Component {
-	constructor() {
-    	super();
-    	this.state = {
-      		monsters: [],
-    	}
-  	}
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+    console.log();
+  }
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((users) =>
-				this.setState(
-					() => {
-						return ({ monsters: users });
-					},
-					() => {
-						console.log(this.state);
-					}
-			 	) 
-			)
-	}
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
 
-	render () {
-    	return (
-      		<div className="App">
-        	{
-          		this.state.monsters.map((monster) => {
-            	return <div key={monster.id}><h1>{monster.name}</h1></div>;
-          	})
-        	}
-      		</div>
-    	);
-  	}
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-
 export default App;
- 
