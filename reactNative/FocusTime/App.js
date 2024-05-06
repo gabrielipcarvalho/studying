64bacd5bbe0ea63f214f0a151cfba9307f5bc83e
gabriel <gabrielipcarvalho@gmail.com>
2024-05-06 20:21:46 +1000

    Implement a productivity-focused timer app
    This commit is the implementation of a productivity timer app built with React Native. Key functionalities include:
    
    - Setting focus tasks to concentrate on with a dedicated input component.
    - Running a countdown timer that supports start and pause operations for each focus session.
    - Managing and displaying a history of focus sessions to track productivity over time.
    - Customizing the length of focus sessions through interactive timing buttons.
    
    The app utilizes components like `ProgressBar` and `RoundedButton` to enhance the user interface, ensuring a user-friendly and engaging experience. This initial setup lays the foundation for further enhancements and user customization features.

--- reactNative/FocusTime/App.js
+++ reactNative/FocusTime/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
     StyleSheet,
     Text,
@@ -8,11 +9,28 @@ import {
 } from "react-native";
 import { colors } from "./src/utils/colors";
 import { Focus } from "./src/features/Focus";
+import { Timer } from "./src/features/Timer";
+import { FocusHistory } from "./src/features/FocusHistory";
 
 export default function App() {
+    const [currentSubject, setCurrentSubject] = useState(null);
+    const [history, setHistory] = useState([]);
     return (
         <SafeAreaView style={styles.container}>
-            <Focus />
+            {!currentSubject ? (
+                <>
+                    <Focus addSubject={setCurrentSubject} />
+                    <FocusHistory history={history} />
+                </>
+            ) : (
+                <Timer
+                    focusSubject={currentSubject}
+                    onTimerEnd={(subject) => {
+                        setHistory([...history, subject]);
+                    }}
+                    clearSubject={() => setCurrentSubject(null)}
+                />
+            )}
         </SafeAreaView>
     );
 }
--- reactNative/FocusTime/src/features/Focus.js
+++ reactNative/FocusTime/src/features/Focus.js
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { colors } from "../utils/colors";
 import { TextInput } from "react-native-paper";
+import { RoundedButton } from "../components/RoundedButton";
+import { spacing } from "../utils/sizes";
 
-export const Focus = () => {
+export const Focus = ({ addSubject }) => {
     const [subject, setSubject] = useState(null);
     console.log(subject);
     return (
         <View style={styles.container}>
             <View style={styles.inputContainer}>
                 <TextInput
-                    onChangeText={(val) => {
-                        setSubject(val);
-                    }}
+                    onChangeText={setSubject}
                     label="What would you like to focus on?"
+                    style={styles.textInput}
                 />
+                <View style={styles.button}>
+                    <RoundedButton
+                        title="+"
+                        size={50}
+                        onPress={() => addSubject(subject)}
+                    />
+                </View>
             </View>
         </View>
     );
@@ -22,14 +30,18 @@ export const Focus = () => {
 
 const styles = StyleSheet.create({
     container: {
+        paddingTop: spacing.sm,
+    },
+    textInput: {
         flex: 1,
+        marginRight: spacing.sm,
+    },
+    button: {
+        justifyContent: "center",
     },
     inputContainer: {
-        flex: 0.5,
-        padding: 25,
-        justifyContent: "top",
-    },
-    text: {
-        color: colors.white,
+        padding: spacing.lg,
+        justifyContent: "flex-start",
+        flexDirection: "row",
     },
 });
